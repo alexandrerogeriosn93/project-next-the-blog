fb60e4880d6c9d5fd3d1e709ca4dc2c665583880
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-08-19 14:10:29 -0300

    feat: create PostsList component and use Suspense to load posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import PostsList from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import clsx from "clsx";
+import { Suspense } from "react";
 
 export default async function Home() {
   return (
     <div>
-      <SpinLoader className={clsx("h-[500px]", "bg-amber-500")} />
+      <header>
+        <h1 className="py-8 text-center text-6xl font-bold">The Blog</h1>
+      </header>
+      <Suspense fallback={<SpinLoader className="" />}>
+        <PostsList />
+      </Suspense>
+      <footer>
+        <p className="py-8 text-center text-6xl font-bold">&copy; 2025</p>
+      </footer>
     </div>
   );
 }
