d222ebf68e781fe52508978247c23fb7cacd0f94
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-08-22 17:43:55 -0300

    feat: create PostCoverImage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostHeading } from "@/components/PostHeading";
 import PostsList from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -12,15 +11,18 @@ export default async function Home() {
     <Container>
       <Header />
       <section className="group mb-16 grid grid-cols-1 gap-8 sm:grid-cols-2">
-        <Link className="h-full w-full overflow-hidden rounded-xl" href="#">
-          <Image
-            className="h-full w-full object-cover object-center transition group-hover:scale-105"
-            src={"/images/bryen_0.png"}
-            width={1200}
-            height={720}
-            alt="Título do Post"
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: "#",
+          }}
+          imageProps={{
+            src: "/images/bryen_0.png",
+            width: 1200,
+            height: 720,
+            alt: "Título do post",
+            priority: true,
+          }}
+        />
         <div className="flex flex-col gap-4 sm:justify-center">
           <time
             className="block text-sm/tight text-slate-600 dark:text-slate-400"
