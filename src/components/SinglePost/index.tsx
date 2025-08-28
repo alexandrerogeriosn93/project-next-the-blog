73f1ad991283e8a93b5a1a37175f15618714213b
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-08-28 18:53:30 -0300

    feat: create component PostDate, refact component PostSummary and add SinglePost styles css

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,4 +1,8 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
+import Image from "next/image";
+
+import { PostDate } from "../PostDate";
+import { PostHeading } from "../PostHeading";
 
 type SinglePostProps = {
   slug: string;
@@ -8,8 +12,25 @@ export async function SinglePost({ slug }: SinglePostProps) {
   const post = await findPostBySlugCached(slug);
 
   return (
-    <div>
-      <p>{post.title}</p>
-    </div>
+    <article className="mb-16">
+      <header className="group mb-4 flex flex-col gap-4">
+        <Image
+          src={post.coverImageUrl}
+          alt={post.title}
+          width={1200}
+          height={720}
+          className="rounded-xl"
+        />
+
+        <PostHeading url={`/post/${post.slug}`}>{post.title}</PostHeading>
+        <p>
+          {post.author} | <PostDate dateTime={post.createdAt} />
+        </p>
+      </header>
+      <p className="mb-4 text-xl text-slate-600 dark:text-slate-400">
+        {post.excerpt}
+      </p>
+      <div>{post.content}</div>
+    </article>
   );
 }
