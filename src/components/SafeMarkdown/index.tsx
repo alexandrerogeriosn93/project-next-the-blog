f8b15ec73abc3cb147ed9fe1ee71cdd2dbd388e9
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-08-28 21:02:14 -0300

    feat: add SafeMarkdown styles css

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import ReactMarkdown from "react-markdown";
 import rehypeSanitize from "rehype-sanitize";
 import remarkGfm from "remark-gfm";
@@ -8,7 +9,23 @@ type SafeMarkdownProps = {
 
 export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
+    <div
+      className={clsx(
+        "prose",
+        "prose-slate",
+        "prose-a:text-blue-500",
+        "prose-a:hover:text-blue-700",
+        "prose-a:transition",
+        "prose-a:no-underline",
+        "prose-a:hover:underline",
+        "prose-img:mx-auto",
+        "md:prose-lg",
+        "dark:prose-invert",
+        "w-full",
+        "max-w-none",
+        "overflow-hidden",
+      )}
+    >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
